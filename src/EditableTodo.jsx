0d426beb9c24aa7d77536198d95430e37360be1c
James Lee <jimhlee23@gmail.com>
2024-04-04 20:14:56 -0400

    work on editable todo list and editable todo

--- src/EditableTodo.jsx
+++ src/EditableTodo.jsx
@@ -9,46 +9,54 @@ import TodoForm from "./TodoForm";
  * - update(): fn to call to update a todo
  * - remove(): fn to call to remove a todo
  *
+ * State
+ * -isEditing: boolean
+ *
  * EditableTodoList -> EditableTodo -> { Todo, TodoForm }
  */
 
-function EditableTodo() {
+function EditableTodo({ todo, update, remove }) {
+  const [isEditing, setIsEditing] = useState(false);
 
   /** Toggle if this is being edited */
-  function toggleEdit() { }
+  function toggleEdit() {
+    setIsEditing(!isEditing);
+  }
 
   /** Call remove fn passed to this. */
-  function handleDelete() { }
+  function handleDelete() {
+    remove(todo.id);
+  }
 
-  /** Edit form saved; toggle isEditing and update in ancestor. */
-  function handleSave(formData) { }
+  /** When Edit form saved; toggle isEditing and update in ancestor. */
+  function handleSave(formData) {
+    update({...formData});
+    // update({...formData, [id]:todo.id});
+    toggleEdit();
+  }
 
   return (
-      <div className="EditableTodo">
+    <div className="EditableTodo">
+      {isEditing === true ?
+      <TodoForm initialFormData={todo} handleSave={handleSave}/> :
+        <div className="mb-3">
+          <div className="float-end text-sm-end">
+            <button
+              className="EditableTodo-toggle btn-link btn btn-sm"
+              onClick={toggleEdit}>
+              Edit
+            </button>
+            <button
+              className="EditableTodo-delBtn btn-link btn btn-sm text-danger"
+              onClick={handleDelete}>
+              Del
+            </button>
+          </div>
+          <Todo />
+        </div>
 
-                EITHER
-
-                <TodoForm />
-
-                OR
-
-                <div className="mb-3">
-                  <div className="float-end text-sm-end">
-                    <button
-                        className="EditableTodo-toggle btn-link btn btn-sm"
-                        onClick={toggleEdit}>
-                      Edit
-                    </button>
-                    <button
-                        className="EditableTodo-delBtn btn-link btn btn-sm text-danger"
-                        onClick={handleDelete}>
-                      Del
-                    </button>
-                  </div>
-                  <Todo />
-                </div>
-
-      </div>
+      }
+    </div>
   );
 }
 
--- src/TodoApp.jsx
+++ src/TodoApp.jsx
@@ -16,47 +16,56 @@ import TodoForm from "./TodoForm";
  * App -> TodoApp -> { TodoForm, EditableTodoList }
  */
 
-function TodoApp({initialTodos = []}) {
+function TodoApp({ initialTodos = [] }) {
   const [todos, setTodos] = useState(initialTodos);
 
   /** add a new todo to list */
   function create(todo) {
-    let newTodo = {...todo, id: uuid() };
+    let newTodo = { ...todo, id: uuid() };
     setTodos(todos => [...todos, newTodo]);
   }
 
   /** update a todo with updatedTodo */
   function update(updatedTodo) {
+    // update state
+    // only update todo in todo state that has changed
+    //
+    console.log('updatedTodo', updatedTodo)
+    console.log('todos', todos)
+    setTodos(todos => [...todos, updatedTodo])
   }
 
   /** delete a todo by id */
   function remove(id) {
+
   }
 
   return (
-      <main className="TodoApp">
-        <div className="row">
+    <main className="TodoApp">
+      <div className="row">
 
-          <div className="col-md-6">
-            <EditableTodoList /> OR
-            <span className="text-muted">You have no todos.</span>
-          </div>
+        <div className="col-md-6">
+          {todos.length > 0 ?
+          <EditableTodoList todos={todos} update={update} remove={remove}/> :
+            <span className="text-muted">You have no todos.</span>}
+        </div>
 
-          <div className="col-md-6">
-            (if no top todo, omit this whole section)
+        <div className="col-md-6">
+          {todos.length > 0 ?
             <section className="mb-4">
               <h3>Top Todo</h3>
-              <TopTodo />
+              <TopTodo todos={todos} />
             </section>
+            : <span>No todos yet</span>}
 
-            <section>
-              <h3 className="mb-3">Add Nü</h3>
-                <TodoForm handleSave={create}/>
-            </section>
-          </div>
-
+          <section>
+            <h3 className="mb-3">Add Nü</h3>
+            <TodoForm handleSave={create} />
+          </section>
         </div>
-      </main>
+
+      </div>
+    </main>
   );
 }
 
